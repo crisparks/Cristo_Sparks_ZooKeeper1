7a50e82df6f41f6ef9fdd14a51b4123a3858540e
Cristo Sparks <50341397+crisparks@users.noreply.github.com>
2019-06-23 11:37:41 -0500

    Add files via upload

---
+++ ZooKeeper/src/GorillaTest.java
@@ -0,0 +1,16 @@
+package src;
+
+
+
+public class GorillaTest
+{
+    public static void main(String[] args){
+	Gorilla test1 = new Gorilla();
+	test1.throwSomething();
+	test1.throwSomething();
+	test1.throwSomething();
+	test1.eatBananas();
+	test1.eatBananas();
+	test1.climb();
+	}
+}
